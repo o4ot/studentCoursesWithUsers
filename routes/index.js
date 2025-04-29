5066f1944a9bf46f81e03c134464be8b9719d286
o4ot <o4ot@fresnou.org>
2025-04-29 08:37:13 -0700

    initial

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,22 @@ var express = require('express');
 var router = express.Router();
 const courseController = require('../controllers/courseController.js');
 const studentController = require('../controllers/studentController.js');
+const userController = require('../controllers/userController.js');
+
+function redirectGuests(req, res, next){
+  if (!req.user){
+    res.redirect('/login');
+    return
+  }
+  next();
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect('/courses');
 });
+
+router.get('/courses', redirectGuests, courseController.viewAll);
 
 router.get('/courses', courseController.viewAll);
 router.get('/courses/profile/:id', courseController.viewProfile);
@@ -21,8 +32,6 @@ router.get('/students', studentController.viewAll);
 router.get('/students/profile/:id', studentController.viewProfile);
 router.get('/students/edit/:id', studentController.renderEditForm);
 router.post('/students/edit/:id', studentController.updateStudent);
-router.get('/students/add', studentController.renderAddForm);
-router.post('/students/add', studentController.addStudent);
 router.get('/students/delete/:id', studentController.deleteStudent);
 
 router.post('/students/:studentId/enroll/', studentController.enrollStudent);
@@ -30,5 +39,12 @@ router.get('/students/:studentId/removeCourse/:courseId', studentController.remo
 router.post('/courses/:courseId/enroll', courseController.enrollStudent);
 router.get('/courses/:courseId/removeStudent/:studentId', courseController.removeStudent);
 
+router.get('/register-student', userController.renderStudentRegistrationForm);
+router.post('/register-student', userController.registerStudent);
+router.get('/register-staff', userController.renderStaffRegistrationForm);
+router.post('/register-staff', userController.registerStaff);
 
+router.get('/login', userController.renderLoginForm);
+router.post('/login', userController.login);
+router.get('/logout', userController.logout);
 module.exports = router;
